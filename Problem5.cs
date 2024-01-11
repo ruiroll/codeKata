129bb783ef99b5400aefd26d437edfcaaf7ea405
ruiroll <154485952+ruiroll@users.noreply.github.com>
2024-01-11 09:31:05 +0900

    Add files via upload

---
+++ Problem5.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class Solution
+{
+    public int solution(int num1, int num2)
+    {
+        int answer = -1;
+        if (num1 == num2)
+        {
+            answer = 1;
+        }
+        return answer;
+    }
+}
